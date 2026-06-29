abe644e21d009a21d5c64128d9760ef76dcf6f98
Seu Nome <sousamateusv@gmail.com>
2026-06-29 20:15:11 -0300

    correção nas rotas

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const userRoutes = Router();
 
 const userController = new UserController();
 
 userRoutes.post("/", userController.create);
-userRoutes.get("/", userController.list);
+userRoutes.get("/", authMiddleware, userController.list);
 
 export { userRoutes };
